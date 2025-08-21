90a210c6f6abda3ad3d848c2226876f1f7efd06f
MarsEtSoleil <dev_android@yahoo.co.jp>
2025-08-21 15:47:01 +0900

    Init with sqlite auto-create tables

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ if (!fs.existsSync(uploadDir)) {
 }
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('myblog.db');
-
+/*
+git add .
+git commit -m "Init with sqlite auto-create tables"
+git push origin main
+*/
 // 初期テーブル作成
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS rireki (
